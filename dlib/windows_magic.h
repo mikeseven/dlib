715ef0d05dec7bcd87d3a44a8b5c4e02dbdb9471
Mikael Bourges-Sevenier <mikeseven@gmail.com>
2015-11-09 23:26:04 -0800

    removed dumb hack on WINNT version as this screws other libraries.

--- dlib/windows_magic.h
+++ dlib/windows_magic.h
@@ -1,14 +1,14 @@
 // Copyright (C) 2006  Davis E. King (davis@dlib.net)
 // License: Boost Software License   See LICENSE.txt for the full license.
-#ifndef DLIB_WINDOWS_MAGIc_ 
-#define DLIB_WINDOWS_MAGIc_ 
+#ifndef DLIB_WINDOWS_MAGIc_
+#define DLIB_WINDOWS_MAGIc_
 
 #include "platform.h"
 
 #ifdef WIN32
 
 // This file contains all the magical #defines you have to setup  before you
-// include the windows header files.  
+// include the windows header files.
 
 #ifndef NOMINMAX
 #define NOMINMAX // prevent windows from messing with std::min and std::max
@@ -24,12 +24,12 @@
 #undef min
 #endif
 
-#ifdef max 
-#undef max 
+#ifdef max
+#undef max
 #endif
 
 #ifdef NO_MAKEFILE
-// only define this if all the cpp files are going to be sucked into the headers 
+// only define this if all the cpp files are going to be sucked into the headers
 // because otherwise we don't need it since everything is isolated in the sockets
 // cpp file and this declaration for _WINSOCKAPI_ appears there also.
 #ifndef _WINSOCKAPI_
@@ -39,10 +39,12 @@
 
 // This is something stupid you have to do to make visual studio include the right
 // stuff.  I don't really know what the deal is with this.
-#if _WIN32_WINNT < 0x0500
+
+// [mbs] don't do that, this screws other libraries!
+/*#if _WIN32_WINNT < 0x0500
 #undef _WIN32_WINNT
 #define _WIN32_WINNT 0x0500
-#endif
+#endif*/
 
 #endif // WIN32
 
